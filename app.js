7d0f0f525425f409139643d67c0f6727bfab1134
Arpit <rpittrer@gmail.com>
2018-06-18 18:04:13 +0530

    third

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hey');
+var time  = 0;
+var timer = setInterval(function(){
+    time += 2;
+    console.log(time + "seconds have passed");
+    if(time>10){
+        clearInterval(timer);
+    }
+}, 2000);
