928d662eb47e0b5662a91f8905a68eac37e198c0
Arpit <rpittrer@gmail.com>
2018-06-18 18:14:48 +0530

    fourth

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var time  = 0;
-var timer = setInterval(function(){
-    time += 2;
-    console.log(time + "seconds have passed");
-    if(time>10){
-        clearInterval(timer);
-    }
-}, 2000);
+console.log('hey');
+
+// var time  = 0;
+// var timer = setInterval(function(){
+//     time += 2;
+//     console.log(time + "seconds have passed");
+//     if(time>10){
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+console.log(__dirname);
+
+console.log(__filename);
+
+
+function call(fun){
+    fun();
+};
+
+var saybye = function(){
+    console.log("bye");
+};
+
+call(saybye);
