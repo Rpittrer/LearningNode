fb953f0063bf715a384617663c621217617cb512
Arpit <rpittrer@gmail.com>
2018-06-21 00:50:06 +0530

    addNote works

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ console.log('starting App.js');
 
 const fs = require('fs');
 const _ = require('lodash');
-const yarg = require('yargs');
-
+const yargs = require('yargs');
 const notes = require('./notes');
 
+var argv = yargs.argv;
 
-var argv = yarg.argv;
+
 var command = process.argv[2];
 console.log('Command: ', command);
 console.log('Process: ', process.argv);
 console.log('Yargs', argv);
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
-    console.log(argv.title);
+    var note = notes.addNote(argv.title, argv.body);
+    console.log(typeof(note));
+    if(note){
+        console.log("Added note!");
+        console.log(`Title: ${note.title} \nBody: ${note.body}`);
+    } else{
+        console.log("Note with same title already exists.");
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
--- notes.js
+++ notes.js
@@ -1,17 +1,32 @@
 console.log("starting Notes.js");
 const fs = require('fs');
 
+var fetchNotes = () => {
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		return JSON.parse(notesString);
+	} catch (e) {
+		return [];
+	}
+};
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-	var notes = [];
+	var notes = fetchNotes();
 	var note = {
 		title,
 		body
 	};
-
-	notes.push(note);
-	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-
-}
+	var noteDuplicates = notes.filter((note) => note.title === title);
+	if (noteDuplicates.length === 0) {
+		notes.push(note);
+		saveNotes(notes);
+		return note;
+	}
+};
 
 var getAll = () => {
 	console.log('getting all notes.');
